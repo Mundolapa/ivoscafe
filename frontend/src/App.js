88bbdc9a23dd20d8504adf2d21c541807b8a2635
Arnold Lara <arnold.lara@gmail.com>
2023-07-10 01:47:12 -0600

    Font-end main files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ScrollToTop from "./helpers/scroll-top";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProtectedRoute from "./helpers/ProtectedRoute";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Home page
+const HomeIvosCafe = lazy(() => import("./pages/home/HomeIvosCafe"));
+
+// Shop pages
+const ShopGridStandard = lazy(() => import("./pages/shop/ShopGridStandard"));
+
+// product pages
+const Product = lazy(() => import("./pages/shop-product/Product"));
+
+// other pages
+const About = lazy(() => import("./pages/other/About"));
+const Contact = lazy(() => import("./pages/other/Contact"));
+const MyAccount = lazy(() => import("./pages/other/MyAccount"));
+const LoginRegister = lazy(() => import("./pages/other/LoginRegister"));
+const Login = lazy(() => import("./pages/other/Login"));
+const Register = lazy(() => import("./pages/other/Register"));
+
+const Cart = lazy(() => import("./pages/other/Cart"));
+const Wishlist = lazy(() => import("./pages/other/Wishlist"));
+const Compare = lazy(() => import("./pages/other/Compare"));
+const Checkout = lazy(() => import("./pages/other/Checkout"));
+
+const NotFound = lazy(() => import("./pages/other/NotFound"));
+
+const App = () => {
+    const { isAuthenticated } = useSelector((state) => state.auth);
+    return (
+        <Router>
+            <ScrollToTop />
+            <Suspense fallback={<div className="flone-preloader-wrapper">...</div>}>
+                <Routes>
+                    {/* Homepage */}
+                    <Route path="/" element={<HomeIvosCafe />} />
+
+                    {/* Shop pages */}
+                    <Route path="/shop-grid-standard" element={<ShopGridStandard />} />
+
+                    {/* Shop product pages */}
+                    <Route path="/product/:id" element={<Product />} />
+
+                    {/* Other pages */}
+                    <Route path="/about" element={<About />} />
+                    <Route path="/acerca-de" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/contactenos" element={<Contact />} />
+                    <Route path="/login-register" element={<LoginRegister />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+
+                    {/* Private routes */}
+                    <Route
+                        path="/my-account"
+                        element={<ProtectedRoute element={<MyAccount />}
+                                                 isAuthenticated={isAuthenticated}
+                                                 redirectPath="/login" />}
+                    />
+
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/wishlist" element={<Wishlist />} />
+                    <Route path="/compare" element={<Compare />} />
+                    <Route path="/checkout" element={<Checkout />} />
+
+                    <Route path="/404" element={<NotFound />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Suspense>
+        </Router>
+    );
 }
 
 export default App;
