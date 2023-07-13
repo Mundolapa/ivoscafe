30b4933fb34e09480cdcd8971f0d9761a132baeb
Arnold Lara <arnold.lara@gmail.com>
2023-07-13 11:21:16 -0600

    Slider app, permission_classes added to the viewsets, migration files added

--- backend/sliders/viewsets.py
+++ backend/sliders/viewsets.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, authentication
 from sliders.models import Slider
 from sliders.serializers import SliderSerializer
 
@@ -6,3 +6,5 @@ class SliderViewSet(viewsets.ModelViewSet):
     queryset = Slider.objects.all().order_by('order')
     serializer_class = SliderSerializer
     pagination_class = None # Disable pagination for this viewset
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
