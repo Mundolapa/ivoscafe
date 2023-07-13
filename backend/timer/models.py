84b536db375206d82f637f2cc193572b030d5fc6
Arnold Lara <arnold.lara@gmail.com>
2023-07-13 11:19:48 -0600

    Timer app, countdowns model added, migration files added

--- backend/timer/models.py
+++ backend/timer/models.py
@@ -1,3 +1,5 @@
+import time
+from PIL import Image
 from django.db import models
 
 class Timer(models.Model):
@@ -22,3 +24,29 @@ class Timer(models.Model):
         self.minutes = seconds // 60
         self.seconds = seconds % 60
         self.save()
+
+
+def cd_image_path(instance, filename):
+    timestamp = int(time.time())
+    return f"images/countdowns/{timestamp}/{filename}"
+
+class Countdown(models.Model):
+    title = models.CharField(max_length=120)
+    datetime = models.DateTimeField()
+    url = models.CharField(max_length=150)
+    image = models.ImageField(blank=True, null=True, upload_to=cd_image_path) # Needs a method to modify the width and height
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 372 or img.width > 372:
+            output_size = (372, 372)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+    def is_active(self) -> models.BooleanField:
+        return self.active  # True or False
