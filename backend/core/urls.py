cd66fcd0afb88820b953b778b199048f533ec1c7
Arnold Lara <arnold.lara@gmail.com>
2023-07-28 15:54:44 -0600

    health_check Middleware created for AWS, requirements modified, language updated, base views updated, settings and urls updated.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.utils.translation import gettext_lazy as _
+from base.views import RootView, Handler404View, HealthCheckView
 
 
 urlpatterns = [
@@ -27,11 +28,15 @@ urlpatterns = [
 ]
 urlpatterns += i18n_patterns(
     path(_('admin/'), admin.site.urls),
+    path('', RootView.as_view(), name='root'),
     path('api/', include('api.urls', namespace='api')),
     path('timer/', include('timer.urls')),
+    path('ping/', HealthCheckView.as_view(), name='health_check'),
     # path('rosetta/', include('rosetta.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 )
+
+handler404 = Handler404View.as_view()
 
 # handler404 = "main.views.handle_404"
 
